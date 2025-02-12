570c22bfdafdade90ec2e872446814c03d3487b4
Mobin_Developer <mobinheydari.developer@gmail.com>
2025-02-12 19:17:54 +0330

    CategoryViewSet added to the views file.

--- Backend/Server/Blogs/views.py
+++ Backend/Server/Blogs/views.py
@@ -41,3 +41,38 @@ class BlogViewSet(ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = models.BlogCategory.objects.all()
+    serializer_class = serializers.BlogCategorySerializer
+    lookup_field = 'slug'
+
+
+    def list(self, request, *args, **kwargs):
+        instance = self.get_queryset()
+        serializer = self.get_serializer(instance, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, slug, *args, **kwargs):
+        instance = get_object_or_404(models.BlogCategory, slug=slug)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response({'Message': 'Category created sucsesfully.'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, slug, *args, **kwargs):
+        instance = get_object_or_404(models.BlogCategory, slug=slug)
+        serializer = self.get_serializer(data=request.data, instance=instance, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
