41133fabc6e9d8851a6c9dfb28c01f6de27a956a
Mobin_Developer <mobinheydari.developer@gmail.com>
2025-02-13 16:22:29 +0330

    BlogContentViewSet class views file

--- Backend/Server/Blogs/views.py
+++ Backend/Server/Blogs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 
 from rest_framework.views import APIView, Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework import status
 
 from . import serializers, models
@@ -71,6 +71,37 @@ class CategoryViewSet(ModelViewSet):
     def update(self, request, slug, *args, **kwargs):
         instance = get_object_or_404(models.BlogCategory, slug=slug)
         serializer = self.get_serializer(data=request.data, instance=instance, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BlogContentViewSet(ViewSet):
+
+    def list(self, request, slug, *args, **kwargs):
+        queryset = models.BlogContent.objects.filter(blog__slug=slug)
+        serializer = serializers.BlogContentSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, slug, content_id, *args, **kwargs):
+        instance = get_object_or_404(models.BlogContent, blog__slug=slug, id=content_id)
+        serializer = serializers.BlogContentSerializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request, slug, *args, **kwargs):
+        instance = get_object_or_404(models.Blog, slug=slug)
+        serializer = serializers.BlogContentSerializer(data=request.data, context={'blog': instance})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'Message': 'Content created sucsesfully.'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, content_id, *args, **kwargs):
+        instance = get_object_or_404(models.BlogContent, id=content_id)
+        serializer = serializers.BlogContentSerializer(data=request.data, instance=instance, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
