980d3e55a008782936c5589f80fab8de2394b016
Mobin_Developer <mobinheydari.developer@gmail.com>
2025-02-14 15:26:58 +0330

    User model added to the project.

--- Backend/Server/Server/settings.py
+++ Backend/Server/Server/settings.py
@@ -132,3 +132,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Auth user model
+AUTH_USER_MODEL = "User.User"
--- Backend/Server/User/models.py
+++ Backend/Server/User/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 
-# Create your models here.
+
+
+
+class User(AbstractUser, PermissionsMixin):
+    """
+    Custom User model that inherits from Django's AbstractUser  model.
+    """
+    # Email field with unique constraint and required
+    email = models.EmailField(max_length=255, unique=True, blank=False)
+
+    # Username field with unique constraint and required
+    username = models.CharField(max_length=12, unique=True, blank=False)
+
+    # Verified status field with default False
+    is_verified = models.BooleanField(default=False)
+
+    # Active status field with default True
+    is_active = models.BooleanField(default=True)
+
+    # Admin status field with default False
+    is_admin = models.BooleanField(default=False)
+
+    # Joined date field with auto_now_add
+    joined_date = models.DateField(auto_now_add=True)
+
+    # Set USERNAME_FIELD to email
+    USERNAME_FIELD = "email"
+
+    # Set REQUIRED_FIELDS to username and full_name
+    REQUIRED_FIELDS = ["username", "full_name"]
+
+    class Meta:
+        """
+        Meta class for User model.
+        """
+        # Set verbose name to User
+        verbose_name = "User "
+
+        # Set verbose name plural to Users
+        verbose_name_plural = "Users"
+
+        # Set ordering to joined_date
+        ordering = ['joined_date']
+
+    def __str__(self):
+        """
+        Returns a string representation of the User object.
+        """
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        """
+        Returns True if the user has the specified permission.
+        """
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        """
+        Returns True if the user has permission to view the app.
+        """
+        return self.is_admin
+
+    @property
+    def is_staff(self):
+        """
+        Returns True if the user is a staff member.
+        """
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
