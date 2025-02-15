d818be6a7915c9574c0f3560058182ff82a5a64d
Mobin_Developer <mobinheydari.developer@gmail.com>
2025-02-15 20:06:59 +0330

    Profile ViewSet with list, retrieve and update method added to the views.py file in Profiles app.

--- Backend/Server/Profiles/views.py
+++ Backend/Server/Profiles/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Profile
+from .serializers import ProfileSerializer
+
+from User.models import User
+
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for the Profile model.
+    """
+    # Define the queryset for the viewset
+    queryset = Profile.objects.all()
+    # Define the serializer class for the viewset
+    serializer_class = ProfileSerializer
+    # Define the permission classes for the viewset
+    permission_classes = [IsAuthenticated]
+    # Defines the required field for retrieve methods
+    lookup_field = 'user_username'
+
+    # Define the list method for the viewset
+    def list(self, request, *args, **kwargs):
+        """
+        Handle GET requests to retrieve a list of profiles.
+        """
+        # Retrieve the queryset
+        queryset = Profile.objects.all()
+        # Serialize the queryset
+        serializer = self.serializer_class(queryset, many=True)
+        # Return the serialized data
+        return Response(serializer.data)
+
+    # Define the retrieve method for the viewset
+    def retrieve(self, request, user_username, *args, **kwargs):
+        """
+        Handle GET requests to retrieve a single profile.
+        """
+        # Getting the user instance
+        instance = get_object_or_404(User, username=user_username)
+        # Retrieve the profile instance
+        queryset = get_object_or_404(Profile, user=instance)
+        # Serialize the profile instance
+        serializer = self.serializer_class(queryset)
+        # Return the serialized data
+        return Response(serializer.data)
+    
+
+    # Define the update method for the viewset
+    def update(self, request, user_username, *args, **kwargs):
+        # Getting the user instance
+        instance = get_object_or_404(User, username=user_username)
+        # Retrieve the profile instance
+        queryset = get_object_or_404(Profile, user=instance)
+        # Serialize the profile instance
+        serializer = self.serializer_class(queryset, data=request.data, partial=True)
+        # Validate the serializer
+        serializer.is_valid(raise_exception=True)
+        # Update the profile instance
+        serializer.save()
+        # Return the serialized data
+        return Response(serializer.data, status.HTTP_202_ACCEPTED)
