f0b762babd01aa36afb91693774bfe691f3fd9ad
Mobin_Developer <mobinheydari.developer@gmail.com>
2025-02-16 12:32:40 +0330

    Course model and Category added to the Courses app in the models.py file.

--- Backend/Server/Courses/models.py
+++ Backend/Server/Courses/models.py
@@ -1,3 +1,81 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    image = models.ImageField(upload_to="Courses/Category/images", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+class Course(models.Model):
+
+    class CourseStatus(models.TextChoices):
+        IN_PROGRESS = 'INP', 'In Progress'
+        COMPLETED = 'COM', 'Completed'
+
+    class CoursePricingStatus(models.TextChoices):
+        FREE = 'Fr', 'Free'
+        PURCHASABLE = 'Pr', 'Purchasable'
+    
+
+    teacher = models.ForeignKey(
+        'User.User',
+        on_delete=models.CASCADE,
+        related_name='Techer_Courses'
+    )
+
+    students = models.ManyToManyField(
+        'User.User', 
+        related_name='User_Courses', 
+        blank=True
+    )
+
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='Course_Category'
+    )
+
+    poster = models.ImageField(upload_to="Courses/poster/", blank=True, null=True)
+    banner = models.ImageField(upload_to="Courses/banner/", blank=True, null=True)
+
+    introduction = models.FileField(upload_to="Courses/intro/", blank=True, null=True)
+
+    description = models.TextField()
+
+    status = models.CharField(
+        max_length=3,
+        choices=CourseStatus.choices,
+        default=CourseStatus.IN_PROGRESS
+    )
+
+    pricing_status = models.CharField(
+        max_length=2,
+        choices=CoursePricingStatus.choices,
+        default=CoursePricingStatus.PURCHASABLE
+    )
+
+    price = models.BigIntegerField(default=0)
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        verbose_name = "Course"
+        verbose_name_plural = "Courses"
+
+    def __str__(self):
+        return self.title
